585c98752dbdc31be6f53fff704df359047dfb1f
Armel <81472365+Armel-James@users.noreply.github.com>
2025-09-12 14:32:21 +0000

    setup for implementing gantt chart -> create redirect button to project templates to show gantt chart

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import Project from "./Project/page";
 import Home from "./Home/page";
 import Landing from "./Landing/page";
+import Tasks from "./Project/Tasks/page";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/tasks" element={<Tasks />} />
         <Route path="/project" element={<Project />} />
       </Routes>
     </Router>
