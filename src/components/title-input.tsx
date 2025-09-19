9be52106eebe7e0e02eba146ed7964932632df1d
jmerc-dev <jelmarmercado@gmail.com>
2025-09-19 16:23:08 +0800

    created a separate component for the title input

--- src/components/title-input.tsx
+++ src/components/title-input.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import type { Project } from "../types/project";
 
 interface TitleInputProps {
@@ -6,18 +7,26 @@ interface TitleInputProps {
 }
 
 export default function TitleInput({ project, setProject }: TitleInputProps) {
+  const [draftName, setDraftName] = useState<string>("");
+
+  useEffect(() => {
+    setDraftName(project.name);
+  }, [project]);
+
   return (
     <input
       type="text"
       placeholder="Project Name"
       className="font-bold outline-0 text-base h-10 border border-transparent rounded-lg p-1 hover:bg-gray-300 hover:cursor-pointer focus:hover:bg-transparent focus:hover:cursor-auto ring-1 ring-transparent focus:ring-blue-400"
-      value={project?.name ?? ""}
-      onChange={(e) => {
-        setProject((prev) => {
-          return prev
-            ? ({ ...prev, name: e.target.value } as Project)
-            : ({ name: e.target.value } as Project);
-        });
+      value={draftName ?? ""}
+      onChange={(e) => setDraftName(e.target.value)}
+      onClick={() => console.log(project.name)}
+      onBlur={() => {
+        setProject((prev) =>
+          prev
+            ? ({ ...prev, name: draftName } as Project)
+            : ({ name: draftName } as Project)
+        );
       }}
     />
   );
